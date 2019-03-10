0bfe0e0907d104d25f7225120833f51420f1afab
heinkhantlin <hkhant96@gmail.com>
2019-03-10 15:29:26 +0630

    First Commit For test

---
+++ commonjs/app.php
@@ -0,0 +1,6 @@
+<?php
+
+$data = file_get_contents('input.txt');
+
+echo $data;
+echo "Other process";
---
+++ es6-module/math.js
@@ -0,0 +1,10 @@
+export var PI = 3.14;
+
+function add(a,b){
+	return a+b;
+}
+
+export function area(r){
+	return PI * r * r;
+}
+
